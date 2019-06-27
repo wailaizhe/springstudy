fe66e14ec94c614586006fe518a091ac4c91fd64
shiyaning <565854802@qq.com>
2019-06-27 16:43:28 +0800

    整合redis

--- src/main/java/spring/service/impl/UserServiceImpl.java
+++ src/main/java/spring/service/impl/UserServiceImpl.java
@@ -2,12 +2,14 @@ package spring.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import spring.dao.CityDao;
 import spring.dao.UserDao;
 import spring.entity.User;
 import spring.exception.ParameterException;
 import spring.page.PageParams;
 import spring.service.UserService;
+import spring.util.RedisUtil;
 import spring.util.VerifyParamsUtils;
 
 import java.util.List;
@@ -20,6 +22,8 @@ import java.util.List;
  */
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    RedisUtil redisUtil;
    @Autowired
     private CityDao cityDao;
     @Autowired
@@ -36,6 +40,12 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUser(Integer id) throws ParameterException {
         VerifyParamsUtils.notNull(id,"id 不能为空");
+        String value = redisUtil.get("id:" + id, 0);
+        if (StringUtils.isEmpty(value)){
+            redisUtil.set("id:"+id,id+"",0);
+        }
+
+
         return userDao.getUser(id);
     }
 
